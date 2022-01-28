c70da1cd28e9c41683b8e63ab3e850b59badb4fd
Sherry <sherryyue.c@protonmail.com>
2022-01-28 21:39:50 +0800

    介面改版

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var $clockDigit = document.querySelector("#clockDigit");
 var $chooseRecord = document.querySelector("#chooseRecord");
 
 var chartLayout = {
-    title: '用電追蹤',
+    title: '',
     yaxis: {
         title: '每日用量（度）',
         titlefont: { color: 'rgb(174, 186, 177)' },
@@ -19,66 +19,172 @@ var chartLayout = {
     }
 };
 
-var dates, degrees, data, rangeCrossYear, crossYear, prevDate, chartData = [];
-var year, monthRange, dayRange, lastIndex;
-var chosen = record.length - 1;
-
-/**
- * @param {string} rawData 
- */
-function printMd(rawData) {
-    converter = new showdown.Converter({
-        metadata: true,
-        underline: true,
-        ghCodeBlocks: true,
-        openLinksInNewWindow: true,
-        emoji: true,
-        simpleLineBreaks: true,
-        tasklists: true,
-        tables: true,
-        splitAdjacentBlockquotes: true,
-        strikethrough: true,
-        parseImgDimensions: true,
-        omitExtraWLInCodeBlocks: true
-    });
-    showdown.setFlavor('vanilla');
-    const html = converter.makeHtml(rawData);
-    $tableSec.innerHTML = html;
-}
+var fullData, chartData = [];
+var viewScale = 'month', viewFrom, viewUntil;
 
 /**
  * @param {string} mdText
  * @returns {{date:string, degree:string, delta:string}[]}
  */
 let md2arr = (mdText) => {
-    let lines = [],
-        pattern = [];
-    lines = mdText.trim().split(/[\r\n]+/);
-    lines.forEach((line) => {
-        pattern.push(line.replace(/\|/g, " ").trim().split(/\s+/));
-    });
-    return pattern;
+    return mdText
+        .trim()
+        .split(/[\r\n]+/)
+        .map((line) => {
+            let [date, degree, delta] = line.replace(/\|/g, " ").trim().split(/\s+/);
+            if (!delta) delta = 0;
+            return { date: date, degree: Number(degree), delta: Number(delta) };
+        });
 }
 
 /**
  * mmdd to yyyy-mm-dd
- * @param {string} date mmdd
- * @param {boolean} _crossYear 
+ * @param {string} date yyyymmdd
  * @returns {string} yyyy-mm-dd
  */
-let dateFormat = (date, _crossYear) => {
-    _crossYear |= crossYear;
-    // cross year
-    if (prevDate && `${prevDate[0]}${prevDate[1]}` > `${date[0]}${date[1]}`) {
-        _crossYear = true;
-    }
-    let str = `${1 * year + (_crossYear ? 1 : 0)}-${date[0]}${date[1]}-${date[2]}${date[3]}`;
-    prevDate = date;
-    crossYear |= _crossYear;
-    return str;
+let dateFormat = (date) => {
+    return date.replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3');
 }
 
-let init = () => {
+let selectWeekScale = () => {
+    viewScale = 'week';
+    let today = new Date();
+    let fromDate = new Date();
+    fromDate = fromDate.setDate(fromDate.getDate() - 7);
+    fromDate = new Date(fromDate);
+    viewUntil = new Date(`${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`);
+    viewFrom = new Date(`${fromDate.getFullYear()}-${fromDate.getMonth() + 1}-${fromDate.getDate()}`);
+    search(viewFrom, viewUntil);
+    document.querySelectorAll('.timeScale>*').forEach(elm => {
+        elm.classList.remove('selected');
+    });
+    document.querySelector('.timeScale>.week').classList.add('selected');
+
+    let label = '本週';
+    document.querySelector('.timeSelect>.cur').textContent = label;
+}
+
+let selectMonthScale = () => {
+    viewScale = 'month';
+    let today = new Date();
+    let fromDate = new Date();
+    fromDate = fromDate.setMonth(fromDate.getMonth() - 1);
+    fromDate = new Date(fromDate);
+    viewUntil = new Date(`${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`);
+    viewFrom = new Date(`${fromDate.getFullYear()}/${fromDate.getMonth() + 1}/${fromDate.getDate()}`);
+    search(viewFrom, viewUntil);
+    document.querySelectorAll('.timeScale>*').forEach(elm => {
+        elm.classList.remove('selected');
+    });
+    document.querySelector('.timeScale>.month').classList.add('selected');
+
+    let label = '本月';
+    document.querySelector('.timeSelect>.cur').textContent = label;
+}
+
+let selectSeasonScale = () => {
+    viewScale = 'season';
+    let today = new Date();
+    let fromDate = new Date();
+    fromDate = fromDate.setMonth(fromDate.getMonth() - 3);
+    fromDate = new Date(fromDate);
+    viewUntil = new Date(`${today.getFullYear()}-/${today.getMonth() + 1}/${today.getDate()}`);
+    viewFrom = new Date(`${fromDate.getFullYear()}/${fromDate.getMonth() + 1}/${fromDate.getDate()}`);
+    search(viewFrom, viewUntil);
+    document.querySelectorAll('.timeScale>*').forEach(elm => {
+        elm.classList.remove('selected');
+    });
+    document.querySelector('.timeScale>.season').classList.add('selected');
+
+    let label = '本季';
+    document.querySelector('.timeSelect>.cur').textContent = label;
+}
+
+let selectPrevScale = () => {
+    viewUntil = viewFrom;
+    viewUntil = viewUntil.setDate(viewFrom.getDate() - 1);
+    viewUntil = new Date(viewUntil);
+    if (viewScale == 'week') viewFrom = viewFrom.setDate(viewUntil.getDate() - 7);
+    if (viewScale == 'month') viewFrom = viewFrom.setMonth(viewUntil.getMonth() - 1);
+    if (viewScale == 'season') viewFrom = viewFrom.setMonth(viewUntil.getMonth() - 3);
+    if (new Date(viewFrom) < new Date(dateFormat(fullData[0].date))) {
+        viewFrom = new Date(dateFormat(fullData[0].date));
+        if (viewScale == 'week') viewUntil = viewUntil.setDate(viewFrom.getDate() + 7);
+        if (viewScale == 'month') viewUntil = viewUntil.setMonth(viewFrom.getMonth() + 1);
+        if (viewScale == 'season') viewUntil = viewUntil.setMonth(viewFrom.getMonth() + 3);
+        viewFrom = new Date(viewFrom);
+        viewUntil = new Date(viewUntil);
+        search(viewFrom, viewUntil);
+        return;
+    }
+    viewFrom = new Date(viewFrom);
+    viewUntil = new Date(viewUntil);
+    search(viewFrom, viewUntil);
+
+    let label = '';
+    if (viewScale == 'week' || viewScale == 'month') {
+        let center = new Date(viewFrom.getTime() + (viewUntil.getTime() - viewFrom.getTime()) / 2);
+        label = `${center.getFullYear()}年 ${center.getMonth() + 1}月`;
+    } else if (viewScale == 'season') {
+        let center = new Date(viewFrom.getTime() + (viewUntil.getTime() - viewFrom.getTime()) / 2);
+        label = `${center.getFullYear()}年`;
+    }
+    document.querySelector('.timeSelect>.cur').textContent = label;
+}
+
+let selectCurScale = () => {
+    viewFrom = new Date();
+    viewUntil = new Date();
+    viewUntil = viewUntil.setDate(viewUntil.getDate() - 1);
+    viewUntil = new Date(viewUntil);
+    if (viewScale == 'week') viewFrom = viewFrom.setDate(viewFrom.getDate() - 7);
+    if (viewScale == 'month') viewFrom = viewFrom.setMonth(viewFrom.getMonth() - 1);
+    if (viewScale == 'season') viewFrom = viewFrom.setMonth(viewFrom.getMonth() - 3);
+    viewFrom = new Date(viewFrom);
+    viewUntil = new Date(viewUntil);
+    search(viewFrom, viewUntil);
+
+    let label = '';
+    if (viewScale == 'week') label = '本週';
+    if (viewScale == 'month') label = '本月';
+    if (viewScale == 'season') label = '本季';
+    document.querySelector('.timeSelect>.cur').textContent = label;
+}
+
+let selectNextScale = () => {
+    viewFrom = viewUntil;
+    viewFrom = viewFrom.setDate(viewUntil.getDate() + 1);
+    viewFrom = new Date(viewFrom);
+    if (viewScale == 'week') viewUntil = viewUntil.setDate(viewFrom.getDate() + 7);
+    if (viewScale == 'month') viewUntil = viewUntil.setMonth(viewFrom.getMonth() + 1);
+    if (viewScale == 'season') viewUntil = viewUntil.setMonth(viewFrom.getMonth() + 3);
+    if (new Date(viewUntil) > new Date()) {
+        viewUntil = new Date();
+        if (viewScale == 'week') viewFrom = viewFrom.setDate(viewUntil.getDate() - 7);
+        if (viewScale == 'month') viewFrom = viewFrom.setMonth(viewUntil.getMonth() - 1);
+        if (viewScale == 'season') viewFrom = viewFrom.setMonth(viewUntil.getMonth() - 3);
+        viewFrom = new Date(viewFrom);
+        viewUntil = new Date(viewUntil);
+        search(viewFrom, viewUntil);
+        return;
+    }
+    viewFrom = new Date(viewFrom);
+    viewUntil = new Date(viewUntil);
+    search(viewFrom, viewUntil);
+
+    let label = '';
+    if (viewScale == 'week' || viewScale == 'month') {
+        let center = new Date(viewFrom.getTime() + (viewUntil.getTime() - viewFrom.getTime()) / 2);
+        label = `${center.getFullYear()}年 ${center.getMonth() + 1}月`;
+    } else if (viewScale == 'season') {
+        let center = new Date(viewFrom.getTime() + (viewUntil.getTime() - viewFrom.getTime()) / 2);
+        label = `${center.getFullYear()}年`;
+    }
+    document.querySelector('.timeSelect>.cur').textContent = label;
+}
+
+let search = (beginDate, endData) => {
+    console.log('search range', beginDate, endData)
     deltaData = {
         x: [],
         y: [],
@@ -94,50 +200,34 @@ let init = () => {
         name: '電表讀數（度）',
         line: { color: 'rgb(58, 62, 59)' }
     };
-    data = md2arr(record[chosen].rawData);
-    year = record[chosen].startYear;
-    crossYear = false;
-    rangeCrossYear = false;
-    let prevDate = data[0][1];
-    // fetch from second line of markdown table
-    data = data.filter((o, i) => { return i > 1 ? true : false });
-    data.forEach((o, i) => {
+    fullData = md2arr(record);
+    if (!beginDate || !endData) {
+        viewFrom = beginDate = new Date(dateFormat(fullData[0].date));
+        viewUntil = endData = new Date(dateFormat(fullData[fullData.length - 1].date));
+    }
+    let filterData = fullData.filter(data => new Date(dateFormat(data.date)) >= beginDate && new Date(dateFormat(data.date)) <= endData);
+
+    let prevDate = filterData[0].degree;
+    filterData.forEach((o, i) => {
         // date
-        let date = dateFormat(o[0], crossYear);
+        let date = dateFormat(o.date);
         degreeData.x.push(date);
         deltaData.x.push(date);
         // degree
-        degreeData.y.push(o[1]);
+        degreeData.y.push(o.degree);
         // delta
-        let delta = (o[1] - (data[i - 1]?.[1] ?? o[1])) / ((new Date(date) - new Date(prevDate ?? date)) / 86400000);
+        let delta = (o.degree - (filterData[i - 1]?.degree ?? o.degree)) / ((new Date(date) - new Date(prevDate ?? date)) / 86400000);
         deltaData.y.push(delta ?? 0);
         prevDate = date;
     });
-    // show table
-    // printMd(record[chosen].rawData);
     // show chart
     Plotly.newPlot('chart', [deltaData, degreeData], chartLayout);
-    // show statics
-    lastIndex = data.length - 1;
-    degreeRange = data[lastIndex][1] - data[0][1];
-    if (data[0][0][0] + data[0][0][1] > data[lastIndex][0][0] + data[lastIndex][0][1]) rangeCrossYear = true;
-    crossYear = false;
-    monthRange = (new Date(dateFormat(data[lastIndex][0], rangeCrossYear)).getTime() - new Date(dateFormat(data[0][0], false)).getTime() + (crossYear ? 86400000 * 30.5 * 12 : 0)) / (86400000 * 30.5);
-    crossYear = false;
-    dayRange = (new Date(dateFormat(data[lastIndex][0], rangeCrossYear)).getTime() - new Date(dateFormat(data[0][0], false)).getTime() + (crossYear ? 365 * 86400000 : 0)) / 86400000;
-    $clockDigit.innerHTML = `在這${monthRange.toFixed(1)}個月中`;
-    $averageAll.innerHTML = `平均每天用了${(degreeRange / dayRange).toFixed(2)}度`;
+    let fee = (filterData[filterData.length - 1].degree - filterData[0].degree) * 4.5;
+    document.querySelector('.info>.fee').textContent = `電費\n$${fee}`;
 }
 
 window.onload = function () {
-    init();
-    record.forEach((it, i) => {
-        $chooseRecord.insertAdjacentHTML("beforeEnd", `<option value="${i}" ${(i == record.length - 1) ? "selected" : ""}>${it.name}</option>`);
-    });
-    $chooseRecord.addEventListener("change", () => {
-        chosen = $chooseRecord.value;
-        init();
-    });
+    selectMonthScale();
 };
 
 // register service worker
--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["app.js","a144d47445ae5ae023dfe4d2a1ade18b"],["index.html","18db98af9a6a84cdc8340c68d598eb95"],["record/2020_Q3Q4.js","88e23321b8eec43f67b3b062aba0c958"],["record/2021_Q1Q2.js","c62a63f2ec6899fa23f3ddf0feea21e5"],["record/2021_Q3Q4.js","b46cc30edbe215b3314f8cd47539780d"],["record/2022_Q1Q2.js","1b889e344f420271f312cb481c64d227"],["style.css","8540897cd0507c94730a9b82a2a878b3"]];
+var precacheConfig = [["app.js","5955f6771490b90775be69f0e2d98daf"],["index.html","8a283e1733809ef09101c7b345934f57"],["record/all.js","e079e27d793699be6def4a7cacaba24c"],["style.css","a5f4a816ce05dff87d4e8f2855d98dd6"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
