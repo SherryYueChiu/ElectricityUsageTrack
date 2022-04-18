b6895f36e7c6f6243fde4936bb375e93578359c5
Sherry <sherry@lulainno.com>
2022-04-18 21:14:19 +0800

    update degree

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["app.js","03c16ddf13751abb634bf5258b33fb98"],["index.html","381d2d91d5cebea1127c007fc847e3a0"],["record/all.js","cefcf7437449b49e4b348bf348c317e3"],["style.css","e6f1009bc4bfcdfae1bb5b20213e1d28"]];
+var precacheConfig = [["app.js","03c16ddf13751abb634bf5258b33fb98"],["index.html","381d2d91d5cebea1127c007fc847e3a0"],["record/all.js","981f125f8fe6402a29fde62bb2ac9fca"],["style.css","e6f1009bc4bfcdfae1bb5b20213e1d28"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
