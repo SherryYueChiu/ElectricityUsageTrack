2ddf8403c3193d119d929dd68e96c6cfe741a719
Sherry <sherryyue.c@protonmail.com>
2023-10-12 22:03:35 +0800

    update degree

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["app.js","e7a36300a9bd9a353d9fce0ab4adf4a3"],["index.html","0aaa97f26943e1c9b3afa05172c771bd"],["record/all.js","981f125f8fe6402a29fde62bb2ac9fca"],["record/all2.js","b30bb5f9cd58e40ef1f5f1e106fb1b4b"],["style.css","2c8cf58a9f3b53d6c5bec9387d4bb4fb"]];
+var precacheConfig = [["app.js","e7a36300a9bd9a353d9fce0ab4adf4a3"],["index.html","0aaa97f26943e1c9b3afa05172c771bd"],["record/all.js","981f125f8fe6402a29fde62bb2ac9fca"],["record/all2.js","ec76730de6e1dfbc2959be907a3cc160"],["style.css","2c8cf58a9f3b53d6c5bec9387d4bb4fb"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
